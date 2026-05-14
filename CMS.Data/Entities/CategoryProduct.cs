b03527adc6dfeb9d9f432aa510e593089773a704
Trần Trọng Tấn <trantrongtan404@gmail.com>
2026-05-14 10:42:09 +0700

    Add project files.

---
+++ CMS.Data/Entities/CategoryProduct.cs
@@ -0,0 +1,30 @@
+﻿/* Ho va ten: Tran Trong Tan
+ * MSSV: 2123110006
+ * Ngay tao: 14/06/2026
+ * Version: 1.0
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CMS.Data.Entities
+{
+    public class CategoryProduct
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên danh mục không được để trống")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        // Quan hệ: Một danh mục có nhiều sản phẩm
+        public virtual ICollection<Product>? Products { get; set; }
+    }
+
+}
